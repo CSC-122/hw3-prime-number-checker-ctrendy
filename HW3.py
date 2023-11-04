b240389b634d8a1a97968567cc6d8da8179ce723
Charles Trendy <ctrendy@umich.edu>
2023-11-03 20:43:45 -0400

    RAAAAAAAAAAAAH I finally did it

--- HW3.py
+++ HW3.py
@@ -5,14 +5,42 @@
 """
 Inputs, Processes, Outputs (IPO)
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-Input(s):
-Processes:
+Input(s): Number
+Processes: Check number, Loop,
 Output(s):
 """
 
 
 def main():
-    pass  # replace this line with all of your solution code
+    start = "y"
+    print("Prime Number Checker")
+    while (start == "y"):
+        num = input("\nEnter an integer to test: ")
+        prime = True
+        try:
+            n = int(num)
+            if (n > 1):
+                for r in range(2, n):
+                    if ((n % r) == 0):
+                        print(f"{n} is a composite number because it is divisible by {r}")
+                        prime = False
+                        break
+                if prime:
+                    print(f"{n} is a prime number")
+                start = input("Do you want to test another integer (y/n): ")
+                start = start.lower()
+            elif (n == 1):
+                print(f"{n} is not a prime number")
+                start = input("Do you want to test another integer (y/n): ")
+                start = start.lower()
+            else:
+                print("Invalid integer value! Try again")
+                start = "y"
+        except:
+            print("Invalid integer value! Try again")
+            start = "y"
+    print("\nBye!")
+
 
 
 if __name__ == "__main__":
